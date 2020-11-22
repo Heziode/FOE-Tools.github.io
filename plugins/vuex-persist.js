ada84dd283694048f95f59b6bafe121dc58c7a12
Heziode <heziode@protonmail.com>
2020-11-22 17:32:46 +0100

    fix: fix multi-tabs sync
    Fix #339

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,7 +1,39 @@
 import createPersistedState from "vuex-persistedstate";
+import transform from "lodash.transform";
+import isEqual from "lodash.isequal";
+import isObject from "lodash.isobject";
+
+const vuexKey = "vuex";
 
 export default ({ store }) => {
   createPersistedState({
     paths: ["global", "profile"],
   })(store);
+
+  const difference = (object, base) => {
+    function changes(object, base, ancestorKey = "") {
+      return transform(object, function (result, value, key) {
+        if (!isEqual(value, base[key])) {
+          if (isObject(value) && isObject(base[key])) {
+            changes(value, base[key], `${ancestorKey}${ancestorKey.length ? "." : ""}${key}`);
+          } else if (base[key]) {
+            store.set(
+              `${ancestorKey}${ancestorKey.length ? "." : ""}${key}`.replace(".", "/").replace(".", "@"),
+              base[key]
+            );
+          }
+        }
+      });
+    }
+    return changes(object, base);
+  };
+
+  // If is browser context
+  if (typeof window !== "undefined") {
+    window.addEventListener("storage", (storageEvent) => {
+      if (storageEvent.key === vuexKey) {
+        difference(store.state, JSON.parse(storageEvent.newValue));
+      }
+    });
+  }
 };
