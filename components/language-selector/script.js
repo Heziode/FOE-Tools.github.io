7ce74cea1d2f1425a7306d89cfc79b74d7f6fda6
Heziode <heziode@protonmail.com>
2021-01-31 16:30:04 +0100

    style: migrate dropdown (b-dropdown & b-dropdown-item) to Tailwind
    Close #379

--- components/language-selector/script.js
+++ components/language-selector/script.js
@@ -1,5 +1,8 @@
+import { mixin as clickaway } from "vue-clickaway";
+
 export default {
   name: "LanguageSelector",
+  mixins: [clickaway],
   data() {
     const supportedLocales = [];
     for (const key of this.$store.get("supportedLocales")) {
@@ -16,13 +19,53 @@ export default {
     return {
       currentLang: this.$clone(this.$store.get("i18n/locale")),
       supportedLocales,
+      isVisible: false,
+      focusedIndex: 0,
     };
+  },
+  computed: {
+    onHelpToTranslatePage() {
+      return ["HelpToTranslateTheSite"].includes(/^([^_]+)(?:___.+)?$/.exec(this.$route.name)[1]);
+    },
   },
   watch: {
     currentLang(lang) {
       this.$store.set("i18n/locale", lang);
       this.locale = lang;
       this.$router.push(this.switchLocalePath(lang));
+    },
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+    hideDropdown() {
+      this.isVisible = false;
+      this.focusedIndex = 0;
+    },
+    startArrowKeys() {
+      if (this.isVisible) {
+        this.$refs.dropdown.children[0].children[0].focus();
+      }
+    },
+    focusPrevious(isArrowKey) {
+      this.focusedIndex = this.focusedIndex - 1;
+      if (isArrowKey) {
+        this.focusItem();
+      }
+    },
+    focusNext(isArrowKey) {
+      this.focusedIndex = this.focusedIndex + 1;
+      if (isArrowKey) {
+        this.focusItem();
+      }
+    },
+    focusItem() {
+      this.$refs.dropdown.children[this.focusedIndex].children[0].focus();
+    },
+    setLocale(locale) {
+      this.$i18n.setLocale(locale);
+      this.hideDropdown();
     },
   },
 };
