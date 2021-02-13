b5628e2f6de20d87f64480c7e96bd8f3c0292c7a
Heziode <heziode@protonmail.com>
2021-02-13 16:22:39 +0100

    style: migrate dialog (b-dialog) to Tailwind

--- plugins/tailwind-comps.js
+++ plugins/tailwind-comps.js
@@ -1,17 +1,19 @@
 import Vue from "vue";
+import { useModalProgrammatic } from "@/components/t-modal";
+import { useDialogProgrammatic } from "@/components/t-dialog";
 import TAutocomplete from "~/components/t-autocomplete/TAutocomplete";
 import TButton from "~/components/t-button/TButton";
 import TCheckbox from "~/components/t-checkbox/TCheckbox";
 import TInput from "~/components/tailwind-comps/TInput/wrapper/TInput";
 import TLabel from "~/components/t-label/TLabel";
 import TMessage from "~/components/t-message/TMessage";
-import { useModalProgrammatic, TModal } from "~/components/t-modal/index";
 import TSelect from "~/components/t-select/TSelect";
 import * as TTabs from "~/components/tailwind-comps/TTabs";
 import useToast from "~/components/tailwind-comps/TToast";
 
 Vue.prototype.$toast = useToast();
 Vue.prototype.$modal = useModalProgrammatic(Vue);
+Vue.prototype.$dialog = useDialogProgrammatic(Vue);
 
 Vue.component("TAutocomplete", TAutocomplete);
 Vue.component("TButton", TButton);
@@ -19,7 +21,6 @@ Vue.component("TCheckbox", TCheckbox);
 Vue.component("TInput", TInput);
 Vue.component("TLabel", TLabel);
 Vue.component("TMessage", TMessage);
-Vue.component("TModal", TModal);
 Vue.component("TSelect", TSelect);
 Vue.component("TTab", TTabs.TTab);
 Vue.component("TTabs", TTabs.TTabs);
